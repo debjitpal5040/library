9d8d5f7a58f737514be7c14bd3ef6dd2f9ca3c9e
Debjit Pal <debjitpal5040@gmail.com>
2022-11-23 21:04:57 +0530

    polishing

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 """website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,15 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('index', views.index),
     path('about', views.about),
-    path('book',views.book),
-    path('community',views.community),
-    path('gallery',views.gallery),
-    path('member',views.member) ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('book', views.book),
+    path('community', views.community),
+    path('gallery', views.gallery),
+    path('member', views.member)]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- website/views.py
+++ website/views.py
@@ -1,19 +1,26 @@
+"""views.py"""
 from django.shortcuts import render
 
 def about(request):
+    """Renders the about page."""
     return render(request, 'about.html')
 
 def book(request):
+    """Renders the book page."""
     return render(request, 'book.html')
 
 def community(request):
+    """Renders the community page."""
     return render(request, 'community.html')
 
 def gallery(request):
+    """Renders the gallery page."""
     return render(request, 'gallery.html')
 
 def index(request):
+    """Renders the home page."""
     return render(request, 'index.html')
 
 def member(request):
+    """Renders the member page."""
     return render(request, 'member.html')
