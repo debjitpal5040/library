e8eac290a0d108411c72bae43d8066da14e9fcfc
Debjit Pal <debjitpal5040@gmail.com>
2023-01-11 11:42:18 +0530

    Revert "Update settings.py"
    This reverts commit 5bf06baf982f16cc2c39b80002d20b114868689e.

--- website/urls.py
+++ website/urls.py
@@ -28,5 +28,3 @@ urlpatterns = [
     path('community', views.community),
     path('gallery', views.gallery),
     path('member', views.member)]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
